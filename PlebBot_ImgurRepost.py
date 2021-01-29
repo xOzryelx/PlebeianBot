ac5b9f68c3f3f6360f70be9380f83428eb3c8954
Jonas <freak1021@gmx.net>
2021-01-29 08:42:20 +0100

    - remove mark_visited

--- PlebBot_ImgurRepost.py
+++ PlebBot_ImgurRepost.py
@@ -207,11 +207,6 @@ if __name__ == "__main__":
         for submission in subreddit.stream.submissions(skip_existing=True):
             logging.info("detected new post")
             main()
-            try:
-                submission.mark_visited()
-            except PRAWException as e:
-                logging.error("failed to mark submission as visited")
-                logging.error(e)
     except PRAWException as e:
         logging.error("reading submission stream failed")
         logging.error(e)
