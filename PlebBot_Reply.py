dbf53f704c796514cde2a432bea7c962401ddfee
Jonas <freak1021@gmx.net>
2021-02-01 13:23:29 +0100

    - Check if I didn't post this myself

--- PlebBot_Reply.py
+++ PlebBot_Reply.py
@@ -120,66 +120,67 @@ def main():
     # wait for new message
     try:
         for message in reddit.inbox.stream():
-            logging.info("found new message")
-            if message.new:
-                if message.subreddit.display_name == "PlebeianAR":
-                    # process what the message says
-                    if "pleb vote" in message.body.lower():
-                        plebVote(message)
+            if message.submission.author.anme != "PlebeianBot":
+                logging.info("found new message")
+                if message.new:
+                    if message.subreddit.display_name == "PlebeianAR":
+                        # process what the message says
+                        if "pleb vote" in message.body.lower():
+                            plebVote(message)
 
-                    elif "good bot" in message.body.lower():
-                        logging.info("good bot")
-                        try:
-                            message.upvote()
-                            message.reply("Thanks for voting\n\nFeature requests welcome")
-                            message.mark_read()
-                        except PRAWException as e:
-                            logging.error("unable to reply")
-                            logging.error(e)
-
-                    elif "bad bot" in message.body.lower():
-                        logging.info("bad bot")
-                        try:
-                            message.reply("Sorry if you don't like me. Please let me know how I can improve")
-                            message.mark_read()
-                        except PRAWException as e:
-                            logging.error("unable to reply")
-                            logging.error(e)
-
-                    elif "who's your daddy" in message.body.lower():
-                        logging.info("daddy?")
-                        if message.author.name == "xOzryelx":
+                        elif "good bot" in message.body.lower():
+                            logging.info("good bot")
                             try:
-                                message.reply("You are my daddy")
+                                message.upvote()
+                                message.reply("Thanks for voting\n\nFeature requests welcome")
                                 message.mark_read()
                             except PRAWException as e:
                                 logging.error("unable to reply")
                                 logging.error(e)
+
+                        elif "bad bot" in message.body.lower():
+                            logging.info("bad bot")
+                            try:
+                                message.reply("Sorry if you don't like me. Please let me know how I can improve")
+                                message.mark_read()
+                            except PRAWException as e:
+                                logging.error("unable to reply")
+                                logging.error(e)
+
+                        elif "who's your daddy" in message.body.lower():
+                            logging.info("daddy?")
+                            if message.author.name == "xOzryelx":
+                                try:
+                                    message.reply("You are my daddy")
+                                    message.mark_read()
+                                except PRAWException as e:
+                                    logging.error("unable to reply")
+                                    logging.error(e)
+                            else:
+                                try:
+                                    message.reply("u/xOzryelx is my daddy")
+                                    message.mark_read()
+                                except PRAWException as e:
+                                    logging.error("unable to reply")
+                                    logging.error(e)
+
+                        elif "PlebeianBot" in message.body.lower():
+                            try:
+                                message.reply("You called master\n\nWhat can I do for you today?")
+                                message.mark_read()
+                            except PRAWException as e:
+                                logging.error("unable to reply")
+                                logging.error(e)
+
                         else:
-                            try:
-                                message.reply("u/xOzryelx is my daddy")
-                                message.mark_read()
-                            except PRAWException as e:
-                                logging.error("unable to reply")
-                                logging.error(e)
-
-                    elif "PlebeianBot" in message.body.lower():
-                        try:
-                            message.reply("You called master\n\nWhat can I do for you today?")
-                            message.mark_read()
-                        except PRAWException as e:
-                            logging.error("unable to reply")
-                            logging.error(e)
+                            logging.warning("someone did something stupid again on post %s with comment %s from %s", message.submission.id, message.id, message.author.name)
 
                     else:
-                        logging.warning("someone did something stupid again on post %s with comment %s from %s", message.submission.id, message.id, message.author.name)
-
-                else:
-                    try:
-                        message.reply("who dares to call me outside of my dungeon?")
-                    except PRAWException as e:
-                        logging.error("unable to reply")
-                        logging.error(e)
+                        try:
+                            message.reply("who dares to call me outside of my dungeon?")
+                        except PRAWException as e:
+                            logging.error("unable to reply")
+                            logging.error(e)
 
             logging.info("done")
     except PRAWException as e:
